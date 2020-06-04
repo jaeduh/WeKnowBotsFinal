00de10a88653983db944f19cb212809e84a25e02
Jaeduh <jada.g1210@gmail.com>
2020-06-04 08:23:25 -0400

    Add project files.

---
+++ WeKnowBots.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeKnowBots.WebMVC.MvcApplication" Language="C#" %>
---
+++ WeKnowBots.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeKnowBots.WebMVC.Startup))]
+namespace WeKnowBots.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
