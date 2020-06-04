170babe18bf161337064abbffd47e00f115c0596
Jaeduh <jada.g1210@gmail.com>
2020-06-04 09:14:23 -0400

    BotClass

---
+++ WeKnowBots.Data/Bot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeKnowBots.Data
+{
+    public class Bot
+    {
+        [Key]
+        public int BotId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string BotName { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+
+    }
+}
